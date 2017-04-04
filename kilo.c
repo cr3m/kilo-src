f79e2e55dac3a8dc993803e6011a8dbe63f6e303
snaptoken <snaptoken@viewsourcecode.org>
2017-04-04 07:50:00 -0600

    save-as c2

--- kilo.c
+++ kilo.c
@@ -69,6 +69,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
+char *editorPrompt(char *prompt);
 
 /*** terminal ***/
 
@@ -359,7 +360,9 @@ void editorOpen(char *filename) {
 }
 
 void editorSave() {
-  if (E.filename == NULL) return;
+  if (E.filename == NULL) {
+    E.filename = editorPrompt("Save as: %s");
+  }
 
   int len;
   char *buf = editorRowsToString(&len);
